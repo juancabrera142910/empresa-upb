2691ef35c978d769299abf6bc3b796da1e3f6c71
Juan Sebastian Cabrera <jcabrera@rapicredit.com>
2025-04-29 18:58:13 -0500

    borre una coma

--- src/main/java/com/upb/servicio/EmpleadoServicio.java
+++ src/main/java/com/upb/servicio/EmpleadoServicio.java
@@ -2,7 +2,7 @@ package com.upb.servicio;
 
 import com.upb.modelo.entidades.Empleado;
 
-import java.util.List;
+import java.util.List
 
 public interface EmpleadoServicio {
     List<Empleado> getTodosEmpleados();
